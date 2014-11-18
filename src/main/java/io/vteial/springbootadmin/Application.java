74488e19e6c9668013dcf38fdc98eef57d4be790
vteial <vteial@gmail.com>
2014-11-18 16:10:06 +0530

    updating latest changes...

---
+++ src/main/java/io/vteial/springbootadmin/Application.java
@@ -0,0 +1,16 @@
+package io.vteial.springbootadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@EnableSwagger
+@SpringBootApplication
+// @Configuration
+// @ComponentScan
+// @EnableAutoConfiguration
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
---
+++ src/main/java/io/vteial/springbootadmin/web/controllers/DefaultController.java
@@ -0,0 +1,20 @@
+package io.vteial.springbootadmin.web.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class DefaultController {
+
+	final Logger _log = LoggerFactory.getLogger(DefaultController.class);
+
+	@RequestMapping(value = "ping", method = RequestMethod.GET)
+	public @ResponseBody String ping() {
+		return "Ping Pong!";
+	}
+}
